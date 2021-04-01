16909374ace55cab7d69efeac010d479c50bb420
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-01 11:21:16 +0200

    v1.1

---
+++ supermarche/src/ChefRayon.java
@@ -0,0 +1,2 @@
+public class ChefRayon {
+}
---
+++ supermarche/src/Supermarche.java
@@ -0,0 +1,17 @@
+public class Supermarche {
+    private static final int NB_ELEMENT_PAR_CHGT = 5;
+    private static final int RAYON_STOCK_INIT = 50;
+    private static final int RAYON_STOCK_MAX = 50;
+    private static final int TPS_PARCOURS_RAYONS = 200;
+    private static final int TPS_PARCOURS_ENTREPOT = 500;
+    private static final int TAILLE_TAPIS = 20;
+    /**
+     *
+     */
+    private static final int NB_CHARIOTS = 15;
+
+    public static void main(String[] args) {
+
+
+    }
+}
