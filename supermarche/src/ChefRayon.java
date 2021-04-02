0ff9b33d6d5563557cad2a06bcabe2db68f0e9ec
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-02 15:18:01 +0200

    Erwann's changes on Rayon.java

--- supermarche/src/ChefRayon.java
+++ supermarche/src/ChefRayon.java
@@ -1,2 +1,11 @@
 public class ChefRayon {
+
+
+    public ChefRayon(){
+
+    }
+
+    public int getStock(String name){
+        return 1;
+    }
 }
--- supermarche/src/Rayon.java
+++ supermarche/src/Rayon.java
@@ -1,8 +1,4 @@
 public class Rayon {
-    /**
-     * L'index du rayon (numero du rayon)
-     */
-    private int index;
 
     /**
      * Le nombre de produits a l'initialisation
@@ -19,18 +15,15 @@ public class Rayon {
      */
     private String nom;
 
-    public Rayon(int index, String nom, int stockMax, int stock){
-        this.index=index;
+    /**
+     * Le chef de rayon
+     */
+    private ChefRayon chefRayon;
+
+    public Rayon(String nom, int stockMax, int stock){
         this.nom=nom;
         this.stockMax=stockMax;
         this.stock=stock;
-    }
-
-    /**
-     * Retourne l'index du rayon
-     */
-    public int getIndex() {
-        return this.index;
     }
 
     /**
@@ -39,4 +32,38 @@ public class Rayon {
     public String getName() {
         return this.nom;
     }
+
+
+    /**
+     * Le client prend un produit dans le rayon
+     */
+    public synchronized void takeProduct(){
+        while(stock<1){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        stock --;
+        notifyAll();
+    }
+
+    /**
+     * Le chef de rayon rajoute des produits en stock si nécessaire
+     */
+    public synchronized int equilibrage(ChefRayon chefRayon){
+
+        int besoinArticle=this.stockMax-this.stock;
+        int nbAddArticle=Math.min(besoinArticle,chefRayon.getStock(getName()));
+
+        for(int i=0;i > nbAddArticle;i++){
+            this.stock++;
+        }
+
+        notifyAll(); // pour prévenir les clients qui attendent de prendre un article
+        System.out.println("Le chef de rayon a équilibré le rayon "+ getName() +".");
+        return chefRayon.getStock(getName())-nbAddArticle;
+    }
+
 }
