3e39a15cfae00317243eb467a30867d8ea7e4a82
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-03 12:48:31 +0200

    Changes on ChefRayon.java

--- supermarche/src/ChefRayon.java
+++ supermarche/src/ChefRayon.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.Thread.sleep;
+
 public class ChefRayon {
 
     Map<String, Integer> chargement = new HashMap();
@@ -20,15 +22,41 @@ public class ChefRayon {
         return chargement.get(name);
     }
 
+    public void recharge(){
+        for (int i = 0; i < rayons.size(); i++) {
+            Rayon rayon = rayons.get(i);
+            this.chargement.put(rayon.getName(), 5);
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void changeDeRayon(){
+        try {
+            sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void run() {
 
         while (true) {
+
+            // Le chef de Rayon reprend 5 articles pour chaque produit
+            recharge();
+            // Le chef de Rayon va au premier rayon
+            changeDeRayon();
 
             for (int i = 0; i < rayons.size(); i++) {
                 Rayon rayon = rayons.get(i);
                 rayon.setChefRayonSurPlace(true);
                 int nombreDeProduitsDecharges = rayon.equilibrage(this);
                 this.chargement.put(rayon.getName(), chargement.get(rayon.getName())- nombreDeProduitsDecharges);
+                changeDeRayon();
             }
 
         }
