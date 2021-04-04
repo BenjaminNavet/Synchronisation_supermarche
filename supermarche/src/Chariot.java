8ef36e521d261d07b45f63b2d376084f29c31bb3
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-04 12:38:44 +0200

    Erwann's changes on Chariot.class : add out.println for actions

--- supermarche/src/Chariot.java
+++ supermarche/src/Chariot.java
@@ -6,19 +6,23 @@ public class Chariot {
         this.nbChariots=nbChariots;
     }
 
-    public synchronized void prendreChariot(){
+    public synchronized void prendreChariot(Client client){
         while(nbChariots==0){
             try {
+                System.out.println("Le client n°" + client.getIndex()+" (" + client.getNom() +") ne peut plus prendre" +
+                        " de chariot, mise en attente sur Chariot." );
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         nbChariots--;
+        System.out.println("Le client n°" + client.getIndex()+" (" + client.getNom() +") prend un chariot." );
     }
 
-    public synchronized void rendreChariot(){
+    public synchronized void rendreChariot(Client client){
         nbChariots++;
+        System.out.println("Le client n°" + client.getIndex()+" (" + client.getNom() +") rend un chariot." );
         notify();
     }
 }
