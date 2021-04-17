ecbdba5fbdf6fe3bb414b9811b497f5067fcfae9
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-17 17:15:11 +0200

    Erwann's changes on EmployeCaisse : add javadoc

--- supermarche/src/EmployeCaisse.java
+++ supermarche/src/EmployeCaisse.java
@@ -1,13 +1,19 @@
 public class EmployeCaisse extends Thread{
 
+    /**
+     * Caisse sur laquelle l'employé de caisse travaille
+     */
     private final Caisse caisse;
 
     public EmployeCaisse(Caisse caisse) {
         this.caisse = caisse;
     }
 
+    /**
+     * Fonctionnement de l'employé de caisse : il cherche constamment à scanner des articles
+     *  3 phases : avant-consommation (avant de scanner), consommation (scanner), après-production (après avoir scanné)
+     */
     public void run() {
-
         while (true) {
             caisse.avant_cons();
             caisse.cons();
