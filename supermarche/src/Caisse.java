fa1c4f2d7b803116c4c85cd287daee13ac977311
Le Chenadec Erwann <erwann.le-chenadec@etudiant.univ-rennes1.fr>
2021-04-21 21:35:48 +0200

    Version with AccesTapisCaisse.java

--- supermarche/src/Caisse.java
+++ supermarche/src/Caisse.java
@@ -88,42 +88,7 @@ public class Caisse {
     }
 
 
-    /** Paiement d'un client lors de son passage en caisse
-     * @param client : permet d'obtenir l'index du client qui souhaite payer
-     */
-    public synchronized void paiement(Client client) {
-        // On a plusieurs processus en concurence donc on utilise un while.
-        // Quand ils sont réveillés, ils doivent revérifier cette condition.
-        // Mise en attente si l'employé de caisse n'a pas fini de scanner les articles du client ou si le
-        // client n'est pas le premier sur la liste d'attente
-        while(!EmployeCaisseAFiniDeScannerPourUnClient || listeAttentePaiement.get(0)!=client.getIndex()){
-            try {
-                System.out.println("Le client n°" + client.getIndex() +" attend de pouvoir payer.");
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
 
-        // Le client met un temps `tps_paiement` pour payer
-        try {
-            sleep(tps_paiement);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // On réinitialise le booléen EmployeCaisseAFiniDeScannerPourUnClient pour indiquer que l'employé de caisse
-        // n'a pas fini de scanner les articles du client suivant (puisqu'il n'a pas commencé)
-        setEmployeCaisseAFiniDeScannerPourUnClient(false);
-
-        // On supprime le numéro du client de la liste d'attente de paiement car il a payé
-        listeAttentePaiement.remove(0);
-
-        System.out.println("Le client n°" + client.getIndex() +" a payé et quitté la caisse.");
-
-        // On réveille tout le monde car il y a plusieurs processus en concurrence
-        notifyAll();
-    }
 
 
     /**
